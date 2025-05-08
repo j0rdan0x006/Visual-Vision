28e509ead59d3e4a35c3844a5a120103ae59e7f6
RAAD <raadbhatti@gmail.com>
2025-05-08 15:13:15 +0100

    Github actions workflow for azure deployment

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -16,7 +16,7 @@ const ContactForm = ({ existingContact = {}, updateCallback }) => {
       email,
     };
     const url =
-      "http://127.0.0.1:5000/" +
+      "http://127.0.0.1:3000/" +
       (updating ? `update_contact/${existingContact.id}` : "create_contact");
     const options = {
       method: updating ? "PATCH" : "POST",
